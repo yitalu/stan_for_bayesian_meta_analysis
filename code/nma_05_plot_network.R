d14179d7764df1e5d801b26a5fdb1e3c0eeb3856
yitalu <luyita.tw@gmail.com>
2025-08-22 12:34:02 +0200

    update

--- code/nma_05_plot_network.R
+++ code/nma_05_plot_network.R
@@ -16,15 +16,15 @@ plot(network, use.description = TRUE) # Use full treatment names
 png(filename = "./figures/network.png", width = 3200, height = 2000, res = 300)
 plot(network, 
     use.description = TRUE,            # Use full treatment names
-    vertex.color = "#6a2007",             # node color
+    vertex.color = "#865151",             # node color
     vertex.label.color = "gray10",     # treatment label color
     vertex.shape = "sphere",           # shape of the node
     vertex.label.family = "Helvetica", # label font
-    vertex.size = 15,                  # size of the node
+    vertex.size = 12,                  # size of the node
     vertex.label.dist = 3,             # distance label-node center
     vertex.label.cex = 2,              # node label size
     edge.curved = 0.2,                 # edge curvature
-    edge.color = "skyblue"             # edge color
+    edge.color = "pink"             # edge color
 )
 dev.off()
 
